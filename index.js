4d5c5405a12b5781ff70e516b9c01448edc04073
Alexandra Janin <contact.ajanin@gmail.com>
2018-04-01 18:23:56 +0200

    Mozilla code

--- index.js
+++ index.js
@@ -1,93 +1,59 @@
 let express = require("express");
 let webPush = require("web-push");
-let atob = require('atob');
-let bodyParser = require('body-parser');
-let util = require('util');
 
-let app = express();
-let subscribers = [];
+const app = express()
 
-let VAPID_SUBJECT = process.env.VAPID_SUBJECT;
-let VAPID_PUBLIC_KEY = process.env.VAPID_PUBLIC_KEY;
-let VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY;
-
-//Auth secret used to authentication notification requests.
-let AUTH_SECRET = process.env.AUTH_SECRET;
-
-if (!VAPID_SUBJECT) {
-    return console.error('VAPID_SUBJECT environment variable not found.')
-} else if (!VAPID_PUBLIC_KEY) {
-    return console.error('VAPID_PUBLIC_KEY environment variable not found.')
-} else if (!VAPID_PRIVATE_KEY) {
-    return console.error('VAPID_PRIVATE_KEY environment variable not found.')
-} else if (!AUTH_SECRET) {
-    return console.error('AUTH_SECRET environment variable not found.')
+if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
+  console.log("You must set the VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY "+
+    "environment variables. You can use the following ones:");
+  console.log(webPush.generateVAPIDKeys());
+  return;
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 webPush.setVapidDetails(
-    VAPID_SUBJECT,
-    VAPID_PUBLIC_KEY,
-    VAPID_PRIVATE_KEY
+  'https://serviceworke.rs/',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
 );
 
-app.use(express.static('static'));
+const subscriptions = {};
+const pushInterval = 10;
 
-app.post('/subscribe', function (req, res) {
-  let endpoint = req.body['notificationEndPoint'];
-  let publicKey = req.body['publicKey'];
-  let auth = req.body['auth'];
 
-  let pushSubscription = {
-      endpoint: endpoint,
-      keys: {
-          p256dh: publicKey,
-          auth: auth
-      }
-  };
-
-  subscribers.push(pushSubscription);
-
-  res.send('Subscription accepted!');
-});
-
-app.post('/unsubscribe', function (req, res) {
-  let endpoint = req.body['notificationEndPoint'];
-
-  subscribers = subscribers.filter(subscriber => { endpoint == subscriber.endpoint });
-
-  res.send('Subscription removed!');
-});
-
-app.get('/notify/all', function (req, res) {
-  if(req.get('auth-secret') != AUTH_SECRET) {
-      console.log("Missing or incorrect auth-secret header. Rejecting request.");
-      return res.sendStatus(401);
-  }
-
-  let message = req.query.message || `Willy Wonka's chocolate is the best!`;
-  let clickTarget = req.query.clickTarget || `http://www.favoritemedium.com`;
-  let title = req.query.title || `Push notification received!`;
-
-  subscribers.forEach(pushSubscription => {
-      //Can be anything you want. No specific structure necessary.
-      let payload = JSON.stringify({message : message, clickTarget: clickTarget, title: title});
-
-      webPush.sendNotification(pushSubscription, payload, {}).then((response) =>{
-          console.log("Status : "+util.inspect(response.statusCode));
-          console.log("Headers : "+JSON.stringify(response.headers));
-          console.log("Body : "+JSON.stringify(response.body));
-      }).catch((error) =>{
-          console.log("Status : "+util.inspect(error.statusCode));
-          console.log("Headers : "+JSON.stringify(error.headers));
-          console.log("Body : "+JSON.stringify(error.body));
-      });
+function sendNotification(subscription) {
+  webPush.sendNotification(subscription)
+  .then(function() {
+    console.log('Push Application Server - Notification sent to ' + subscription.endpoint);
+  }).catch(function() {
+    console.log('ERROR in sending Notification, endpoint removed ' + subscription.endpoint);
+    delete subscriptions[subscription.endpoint];
   });
+}
 
-  res.send('Notification sent!');
-});
+setInterval(function() {
+  Object.values(subscriptions).forEach(sendNotification);
+}, pushInterval * 1000);
 
 app.listen(3000, function () {
-})
+  app.get('vapidPublicKey', function(req, res) {
+    res.send(process.env.VAPID_PUBLIC_KEY);
+  });
+
+  app.post('register', function(req, res) {
+    var subscription = req.body.subscription;
+    if (!subscriptions[subscription.endpoint]) {
+      console.log('Subscription registered ' + subscription.endpoint);
+      subscriptions[subscription.endpoint] = subscription;
+    }
+    res.type('js').send('{"success":true}');
+  });
+
+  app.post('unregister', function(req, res) {
+    var subscription = req.body.subscription;
+    if (subscriptions[subscription.endpoint]) {
+      console.log('Subscription unregistered ' + subscription.endpoint);
+      delete subscriptions[subscription.endpoint];
+    }
+    res.type('js').send('{"success":true}');
+  });
+});
